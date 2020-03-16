3cc81660ed2ec9b169760bca1637470839e0103c
Alex Leshenko <alex.l@sca.auction>
2020-03-16 12:55:01 +0300

    feat: compelted task Singolo. DOM

---
+++ script.js
@@ -0,0 +1,182 @@
+
+//Slider
+
+let imgPhoneVert = document.querySelector('.phone-vert')
+let imgPhoneGorizont = document.querySelector('.phone-gorizontal')
+imgPhoneVert.addEventListener('click',function (event) {
+    event.currentTarget.classList.toggle('.phone-vert-bg');
+    event.currentTarget.classList.toggle('phone-vert-img');
+})
+
+imgPhoneGorizont.addEventListener('click',function (event) {
+    event.currentTarget.classList.toggle('.phone-gorizontal-bg');
+    event.currentTarget.classList.toggle('phone-gorizontal-img');
+})
+
+
+let sliderContent = document.querySelector('.slider_content')
+for (i=0; i<sliderContent.children.length ;i++){
+    if (i === 0 ){sliderContent.children[i].classList.add('display_active')}
+    if (i === 0 ){continue}
+    sliderContent.children[i].classList.add('display_none')
+}
+
+let arrowNext = document.querySelector('.arrow-next')
+let arrowPrev = document.querySelector('.arrow-prev')
+
+arrowNext.addEventListener('click',function (event) {
+    let active = sliderContent.getElementsByClassName('display_active')[0]
+    if(!!active.nextElementSibling){
+        active.classList.add('display_none')
+        active.classList.remove('display_active')
+        active.nextElementSibling.classList.add('display_active')
+        active.nextElementSibling.classList.remove('display_none')
+    }else {
+        active.classList.add('display_none')
+        active.classList.remove('display_active')
+        sliderContent.firstElementChild.classList.add('display_active')
+        sliderContent.firstElementChild.classList.remove('display_none')
+    }
+})
+
+arrowPrev.addEventListener('click',function (event) {
+    let active = sliderContent.getElementsByClassName('display_active')[0]
+    if(!!active.previousElementSibling){
+        active.classList.add('display_none')
+        active.classList.remove('display_active')
+        active.previousElementSibling.classList.add('display_active')
+        active.previousElementSibling.classList.remove('display_none')
+    }else {
+        active.classList.add('display_none')
+        active.classList.remove('display_active')
+        sliderContent.lastElementChild.classList.add('display_active')
+        sliderContent.lastElementChild.classList.remove('display_none')
+    }
+})
+
+//menu_nav
+
+let navigationLinks = document.querySelector('.navigation')
+
+for (i=0; i<navigationLinks.children.length ;i++){
+    if (i === 0 ){navigationLinks.children[i].classList.add('active')}
+    navigationLinks.children[i].addEventListener('click',function (e) {
+
+        for (i=0; i<navigationLinks.children.length ;i++){
+            navigationLinks.children[i].classList.remove('active')
+        }
+        history.pushState('', "", `#${e.target.textContent}`);
+        e.currentTarget.classList.add('active')
+    })
+}
+
+//Portfolio
+
+let portfolioBottom = document.querySelector('.category-bottom')
+
+function randomInteger(min, max) {
+    // получить случайное число от (min-0.5) до (max+0.5)
+    let rand = min - 0.5 + Math.random() * (max - min + 1);
+    return Math.round(rand);
+}
+
+for (i=0; i<portfolioBottom.children.length ;i++){
+    if (i === 0 ){portfolioBottom.children[i].classList.add('active')}
+    portfolioBottom.children[i].addEventListener('click',function (e) {
+        for (i=0; i<portfolioBottom.children.length ;i++){
+            portfolioBottom.children[i].classList.remove('active')
+        }
+        e.currentTarget.classList.add('active')
+        let galleryCont = document.querySelector('.gallery')
+
+        const divyArray = Array.prototype.slice.call(galleryCont.children)
+
+        let nDivs = document.createDocumentFragment();
+
+        while(divyArray.length){
+            let numb = randomInteger(0, divyArray.length-1)
+                nDivs.appendChild(divyArray[numb])
+                divyArray.splice(numb, 1);
+
+        }
+        galleryCont.prepend(nDivs)
+    })
+}
+
+let imgPorfolio = document.querySelector('.gallery').children
+Array.prototype.forEach.call(imgPorfolio,(item)=>{
+    item.addEventListener('click',function (e) {
+        for (let i=0; i<imgPorfolio.length; i++){
+            imgPorfolio[i].classList.remove('border-color-img')
+        }
+        e.currentTarget.classList.add('border-color-img')
+
+    })
+})
+
+//form submit
+
+let formSubmitBtn = document.querySelector('.form-submit')
+
+let createMessageItem = (label, message ='') =>{
+    let div = document.createElement('p');
+    div.innerHTML = `${label}${message}`;
+    return div
+}
+
+formSubmitBtn.addEventListener('click', function (e) {
+    e.preventDefault()
+    let message = document.createDocumentFragment();
+    let form = e.currentTarget.form
+    let elem = form.querySelectorAll('input')
+    for(let i=0; i < elem.length; i++){
+        elem[i].classList.remove('errors')
+        if (elem[i].required && elem[i].value === ""){
+            elem[i].classList.add('errors')
+        }
+    }
+    if(!form.querySelector('.errors')){
+
+        message.append(createMessageItem('The letter was sent'))
+        for(let i=0; i < elem.length; i++){
+            switch(elem[i].name) {
+                case 'Subject':  // if (x === 'value1')
+                    if (elem[i].value !== ""){
+                        message.append(createMessageItem('Subject: ', elem[i].value))
+                    }else {
+                        message.append(createMessageItem('Without subject'))
+                    }
+                    break
+            }
+        }
+        let descText = form.querySelector('textarea').value
+        if (!!descText){
+            message.append(createMessageItem('Description:', descText))
+        }else {
+            message.append(createMessageItem('Without description'))
+        }
+
+        let button = document.createElement('button');
+        button.innerHTML = 'Ok'
+        button.onclick = closeModal
+
+        message.append(button)
+
+
+        document.querySelector('.modal-wrapper').classList.remove('display_none')
+        document.querySelector('.modal-text').innerHTML = "";
+        document.querySelector('.modal-text').append(message)
+    }else {
+        alert('заполни обязательные поля')
+    }
+})
+
+function closeModal(){
+    document.querySelector('.modal-wrapper').classList.add('display_none')
+}
+
+//modal
+let modalCloseBtn = document.querySelector('.modal-close')
+modalCloseBtn.addEventListener('click', function (e) {
+    closeModal()
+})
